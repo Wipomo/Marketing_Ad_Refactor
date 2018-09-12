5ff8152e7edb41c1d28e8fdf978642c7b4604380
Elliot Jane Lader <elliot.j.lader@gmail.com>
2018-09-12 13:47:39 -0600

    Debugging production DB

--- index.js
+++ index.js
@@ -17,12 +17,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/customers', customers);
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'build/index.html'));
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.post('/generate-email', (req, res) => {
 
@@ -55,6 +51,23 @@ app.post('/generate-email', (req, res) => {
     });
 
 });
+
+// eslint-disable-next-line
+function notFound(req, res, next) {
+    const url = req.originalUrl
+    if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
+        // Don't log less important (automatic) browser requests
+        console.error('[404: Requested file not found] ', url)
+    }
+    res.status(404).send({error: 'Url not found', status: 404, url})
+}
+
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+    console.error('ERROR', err)
+    const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+    res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port);
