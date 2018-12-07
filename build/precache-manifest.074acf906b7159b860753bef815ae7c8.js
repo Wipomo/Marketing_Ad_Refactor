2966e6f69b25cf150292a807cbbbc509600a1f8c
Olasubomi Awokoya <olasubomi.awokoya@hotmail.com>
2018-12-07 13:02:17 -0800

    updating build using yarn to update chunks and not just builds asset-manifest, serviceworker and index files. Running build with yarn is expected to update build and show recent updates

---
+++ build/precache-manifest.074acf906b7159b860753bef815ae7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "247e4099d06672561adb",
+    "url": "/static/js/main.247e4099.chunk.js"
+  },
+  {
+    "revision": "64351caf1b03e4680ec4",
+    "url": "/static/js/1.64351caf.chunk.js"
+  },
+  {
+    "revision": "247e4099d06672561adb",
+    "url": "/static/css/main.81866863.chunk.css"
+  },
+  {
+    "revision": "64351caf1b03e4680ec4",
+    "url": "/static/css/1.aee7ce26.chunk.css"
+  },
+  {
+    "revision": "54c00b20b3a86fbb1ca0d95adb8607d2",
+    "url": "/index.html"
+  }
+];
