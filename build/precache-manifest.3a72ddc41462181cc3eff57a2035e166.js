376f2bb11508596bfbe59b586626c2f79070ce4a
Olasubomi Awokoya <olasubomi.awokoya@hotmail.com>
2019-01-15 11:58:14 -0800

    reverting get chart data refactoring to default fetch algorithm in build

---
+++ build/precache-manifest.3a72ddc41462181cc3eff57a2035e166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "578f06eeefcb875123ac",
+    "url": "/static/js/main.578f06ee.chunk.js"
+  },
+  {
+    "revision": "74bf09736be4161ee6c9",
+    "url": "/static/js/1.74bf0973.chunk.js"
+  },
+  {
+    "revision": "578f06eeefcb875123ac",
+    "url": "/static/css/main.2d53bd36.chunk.css"
+  },
+  {
+    "revision": "74bf09736be4161ee6c9",
+    "url": "/static/css/1.20123b29.chunk.css"
+  },
+  {
+    "revision": "2f5feee8905e3e080670a436cde56685",
+    "url": "/index.html"
+  }
+];
