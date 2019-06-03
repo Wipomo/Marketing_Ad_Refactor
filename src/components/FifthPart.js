67c9578a747d314850f192c69322edb7a3dfd31e
Kairav Suman <kairav.suman@hotmail.com>
2019-06-03 18:15:54 +0530

    SC3.0 Blog tile

--- src/components/FifthPart.js
+++ src/components/FifthPart.js
@@ -72,7 +72,7 @@ class FifthPart extends React.Component {
 
           <section className="blogThumbnails text-center">
             <div className="row no-gutters">
-              <div className="col-md-4 col-lg-2">
+              <div className="col-sm-6 col-md-4 col-lg-2">
                 <div className="thumbnail">
                   <a href="https://www.makello.com/blog/what-the-competition-does-to-cut-corners" target="_blank" rel="noopener noreferrer" className="viewBlogContent">
                     <img src = "images/blog_thumbnails/How_the_Competition_Cuts_Corners.JPG" className="single_blog_thumbnail" alt="How_the_Competition_Cuts_Corners" />
@@ -80,7 +80,7 @@ class FifthPart extends React.Component {
                   </a>
                 </div>
               </div>
-              <div className="col-md-4 col-lg-2">
+              <div className="col-sm-6 col-md-4 col-lg-2">
                 <div className="thumbnail">
                   <a href="https://www.makello.com/blog/vws-microbus-ev-redemption" target="_blank" rel="noopener noreferrer" className="viewBlogContent">
                     <img src = "images/blog_thumbnails/VW_s Microbus_EV_Redemption.JPG" className="single_blog_thumbnail" alt="VW_s Microbus_EV Redemption.JPG" />
@@ -88,7 +88,7 @@ class FifthPart extends React.Component {
                   </a>
                 </div>
               </div> 
-              <div className="col-md-4 col-lg-2">
+              <div className="col-sm-6 col-md-4 col-lg-2">
                 <div className="thumbnail">
                   <a href="https://www.makello.com/blog/dirty_little_secrets_of_solar" target="_blank" rel="noopener noreferrer" className="viewBlogContent">
                     <img src = "images/blog_thumbnails/Dirty_Little_Secrets_of_the_Solar_Industry.jpg" className="single_blog_thumbnail" alt="Buyer's Rights for Energy Upgrades" />
@@ -96,7 +96,7 @@ class FifthPart extends React.Component {
                   </a>
                 </div>
               </div>
-              <div className="col-md-4 col-lg-2">
+              <div className="col-sm-6 col-md-4 col-lg-2">
                 <div className="thumbnail">
                   <a href="https://www.makello.com/blog/buyers-rights" target="_blank" rel="noopener noreferrer" className="viewBlogContent">
                     <img src = "images/blog_thumbnails/Buyers_Rights_for_Energy_Upgrades.jpg" className="single_blog_thumbnail"  alt="Buyer's Rights for Energy Upgrades" />
@@ -104,7 +104,7 @@ class FifthPart extends React.Component {
                   </a>
                 </div>
               </div>
-              <div className="col-md-4 col-lg-2">
+              <div className="col-sm-6 col-md-4 col-lg-2">
                 <div className="thumbnail">
                   <a href="https://www.makello.com/blog/a-quality-installation-lasts-a-lifetime" target="_blank" rel="noopener noreferrer" className="viewBlogContent">
                     <img src = "images/blog_thumbnails/Quality_Installation_Lasts_a_Lifetime.jpg" className="single_blog_thumbnail" alt="Quality Installation Lasts a Lifetime" />
@@ -112,14 +112,14 @@ class FifthPart extends React.Component {
                   </a>
                 </div>
               </div> 
-              <section className="col-md-4 col-lg-2">
+              <div className="col-sm-6 col-md-4 col-lg-2">
                 <div className="thumbnail">
                   <a href="https://www.makello.com/blog/rivian-the-spirit-of-an-adventurer" target="_blank" rel="noopener noreferrer" className="viewBlogContent">
                     <img src = "images/blog_thumbnails/Rivian_the_Spirit_of_an_Adventurer.jpg" className="single_blog_thumbnail" alt="Rivian, the Spirit of an Adventurer.jpg" />
                     <div className="caption">Rivian, the Spirit of an Adventurer</div>
                   </a>
                 </div>
-              </section>
+              </div>
             </div>
           </section>
 
